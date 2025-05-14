6cb1b6e6b9610527b57c691e7a68952a32e8ff5f
Fernando Moreno <morenomoranfernando@gmail.com>
2025-05-13 19:00:08 -0600

    Add vue

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,11 @@
 import { defineConfig } from 'astro/config'
 import clerk from '@clerk/astro'
 import tailwindcss from '@tailwindcss/vite'
+import vue from '@astrojs/vue'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [clerk()],
+  integrations: [clerk(), vue()],
   output: 'server',
   vite: {
     plugins: [tailwindcss()],
