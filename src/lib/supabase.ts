fd57f6139986b5a43f9991216163458ec4c92971
Fernando Moreno <morenomoranfernando@gmail.com>
2025-05-13 19:03:34 -0600

    Add supabaseServer function for server-side client creation with clerk token support

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -4,3 +4,21 @@ export const supabase = createClient(
   import.meta.env.SUPABASE_URL,
   import.meta.env.SUPABASE_ANON_KEY,
 )
+
+export async function supabaseServer(locals: App.Locals) {
+  const { userId, getToken } = locals.auth()
+
+  if (!userId) {
+    throw new Error('No user ID provided')
+  }
+
+  return createClient(
+    import.meta.env.SUPABASE_URL,
+    import.meta.env.SUPABASE_ANON_KEY,
+    {
+      async accessToken() {
+        return await getToken()
+      },
+    },
+  )
+}
