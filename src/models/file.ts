b4cfe1d7092a119ee9b6aee32c3243a68092e72d
Fernando Moreno <morenomoranfernando@gmail.com>
2025-05-16 02:54:07 -0600

    Add File and User interfaces for type definitions

---
+++ src/models/file.ts
@@ -0,0 +1,10 @@
+export interface File {
+  id: string
+  created_by: string
+  bucket_name: string
+  object_path: string
+  file_name: string
+  size: number
+  thumbnail: string
+  created_at: string
+}
---
+++ src/models/user.ts
@@ -0,0 +1,8 @@
+export interface User {
+  id: string
+  email: string
+  username: string
+  first_name: string
+  last_name: string
+  image_url: string
+}
