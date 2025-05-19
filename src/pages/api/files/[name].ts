40123180a7a84d397138751f1b0e2f6cd1b16e24
Fernando Moreno <morenomoranfernando@gmail.com>
2025-05-19 03:10:37 -0600

    Implement MFA check for file downloads and enhance error handling

--- src/pages/api/files/[name].ts
+++ src/pages/api/files/[name].ts
@@ -1,7 +1,10 @@
 import type { APIRoute } from 'astro'
 import { supabaseServer } from '@/lib/supabase'
+import { clerkClient } from '@clerk/astro/server'
 
-export const GET: APIRoute = async ({ params, locals }) => {
+// Handles file downloads
+export const GET: APIRoute = async (context) => {
+  const { params, locals } = context
   const { name } = params
   const { userId } = locals.auth()
 
@@ -18,9 +21,70 @@ export const GET: APIRoute = async ({ params, locals }) => {
 
   const supabase = await supabaseServer(locals)
 
+  const { data: user, error: userError } = await supabase
+    .from('files')
+    .select('object_path, id')
+    .eq('id', name)
+    .single()
+
+  if (userError) {
+    console.error('Error fetching file:', userError)
+    return new Response(JSON.stringify({ error: 'Error fetching file' }), {
+      status: 500,
+      headers: { 'Content-Type': 'application/json' },
+    })
+  }
+
+  // Check if the file-share needs MFA to download it
+
+  const { data: fileShare, error: fileShareError } = await supabase
+    .from('shares')
+    .select('requires_mfa, users(id)')
+    .eq('file_id', user.id)
+    .eq('users.id', userId)
+    .limit(1)
+    .single()
+
+  if (fileShareError) {
+    console.error('Error fetching file share:', fileShareError)
+    return new Response(
+      JSON.stringify({ error: 'Error fetching file share' }),
+      {
+        status: 500,
+        headers: { 'Content-Type': 'application/json' },
+      },
+    )
+  }
+
+  // If MFA is required, check if the user has MFA enabled
+  if (fileShare.requires_mfa) {
+    try {
+      const clerkUser = await clerkClient(context).users.getUser(userId)
+
+      if (!clerkUser) {
+        return new Response('Unauthorized', { status: 401 })
+      }
+
+      if (!clerkUser.twoFactorEnabled) {
+        return new Response(JSON.stringify({ error: 'MFA is required' }), {
+          status: 403,
+          headers: { 'Content-Type': 'application/json' },
+        })
+      }
+    } catch (e) {
+      console.error('Error fetching MFA:', e)
+      return new Response(JSON.stringify({ error: 'Error fetching MFA' }), {
+        status: 500,
+        headers: { 'Content-Type': 'application/json' },
+      })
+    }
+  }
+
+  const { object_path } = user
+
   const { data, error } = await supabase.storage
     .from('userfiles')
-    .download(`${userId}/${name}`)
+    .download(object_path)
 
   if (error) {
     console.error('Error downloading file:', error)
