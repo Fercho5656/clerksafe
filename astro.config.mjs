ff211e7fe2f01b8162317ffb1fc5b867917d7f27
Fernando Moreno <morenomoranfernando@gmail.com>
2025-05-20 01:14:01 -0600

    Add Vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,15 +3,20 @@ import { defineConfig } from 'astro/config'
 import clerk from '@clerk/astro'
 import tailwindcss from '@tailwindcss/vite'
 import vue from '@astrojs/vue'
+import vercel from '@astrojs/vercel'
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [clerk(), vue()],
   output: 'server',
+
   server: {
     allowedHosts: true
   },
+
   vite: {
     plugins: [tailwindcss()],
   },
+
+  adapter: vercel(),
 })
